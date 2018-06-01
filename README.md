f21fcbbb5bc924839a90bdb3e528569900eb6f1c
Maciej Malecki <smt116@gmail.com>
2018-06-01 08:49:54 +0200

    Allow loading native file from the root
    This option is useful when cannot rely on the `__dirname` when it points
    to the `node_modules`'s subdirectory.
    
    [#3]

--- README.md
+++ README.md
@@ -20,10 +20,12 @@ Update rules entry in the Webpack configuration file:
 
 ```js
 module: {
-  rules: [{
-    test: /\.node$/,
-    loader: 'native-ext-loader'
-  }]
+  rules: [
+    {
+      test: /\.node$/,
+      loader: "native-ext-loader"
+    }
+  ];
 }
 ```
 
@@ -33,13 +35,15 @@ Options are configurable using `options` hash:
 
 ```js
 module: {
-  rules: [{
-    test: /\.node$/,
-    loader: 'native-ext-loader',
-    options: {
-      rewritePath: path.resolve(__dirname, 'dist')
+  rules: [
+    {
+      test: /\.node$/,
+      loader: "native-ext-loader",
+      options: {
+        rewritePath: path.resolve(__dirname, "dist")
+      }
     }
-  }]
+  ];
 }
 ```
 
@@ -49,11 +53,15 @@ It allows to set an absolute paths to native files.
 
 Note that it needs to remain `undefined` if you are building a package with embedded files. This way, the compiled application will work no matter of its location. This is important when building Electron applications that can be placed in any directory by the end user.
 
+### `loadFromRoot` (default: `undefined`)
+
+It allows to load the file as it is from the root. This option is useful when cannot rely on the `__dirname` when it points to the `node_modules`'s subdirectory.
+
 ## Releasing a new version
 
-1. Bump version number in the `package.json` and `CHANGELOG.md` files.
-1. Run `npm install` to update `package-lock.json` file.
-1. Commit changes (include changes)
-1. Add a new tag (use `-a` and include changes)
-1. Push commits and tag
-1. Run `npm publish`
+1.  Bump version number in the `package.json` and `CHANGELOG.md` files.
+1.  Run `npm install` to update `package-lock.json` file.
+1.  Commit changes (include changes)
+1.  Add a new tag (use `-a` and include changes)
+1.  Push commits and tag
+1.  Run `npm publish`
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var path = require("path");
 
-module.exports = function (content) {
+module.exports = function(content) {
   const defaultConfig = {
     rewritePath: undefined
   };
@@ -12,19 +12,46 @@ module.exports = function (content) {
     this.emitFile(fileName, content, false);
     this.addDependency(this.resourcePath);
   } else {
-    throw new Error('emitFile function is not available');
+    throw new Error("emitFile function is not available");
   }
 
-  if (config.rewritePath) {
+  if (config.rewritePath && config.loadFromRoot) {
+    throw new Error(
+      "cannot use `rewritePath` and `loadFromRoot` at the same time"
+    );
+  }
+
+  if (config.loadFromRoot) {
+    const filePath = JSON.stringify(fileName);
+
+    return (
+      "try { global.process.dlopen(module, " +
+      filePath +
+      "); } " +
+      "catch(exception) { throw new Error('Cannot open ' + " +
+      filePath +
+      " + ': ' + exception); };"
+    );
+  } else if (config.rewritePath) {
     const filePath = JSON.stringify(path.join(config.rewritePath, fileName));
 
-    return "try { global.process.dlopen(module, " + filePath + "); } " +
-      "catch(exception) { throw new Error('Cannot open ' + " + filePath + " + ': ' + exception); };";
+    return (
+      "try { global.process.dlopen(module, " +
+      filePath +
+      "); } " +
+      "catch(exception) { throw new Error('Cannot open ' + " +
+      filePath +
+      " + ': ' + exception); };"
+    );
   } else {
-    return "const path = require('path');" +
-      "const filePath = path.resolve(__dirname, " + JSON.stringify(fileName) + ");" +
+    return (
+      "const path = require('path');" +
+      "const filePath = path.resolve(__dirname, " +
+      JSON.stringify(fileName) +
+      ");" +
       "try { global.process.dlopen(module, filePath); } " +
-      "catch(exception) { throw new Error('Cannot open ' + filePath + ': ' + exception); };";
+      "catch(exception) { throw new Error('Cannot open ' + filePath + ': ' + exception); };"
+    );
   }
 };
 
