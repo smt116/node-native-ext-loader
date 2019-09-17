b51f32e78f53e226b7762756cb691193706edc43
William Gallafent <wgallafent@oni.bio>
2019-09-17 12:28:22 +0100

    Simplify for our more specialised use case, fix for relative paths

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var path = require("path");
 module.exports = function(content) {
   const defaultConfig = {
     basePath: [],
-    rewritePath: undefined,
     emit: true
   };
 
@@ -20,36 +19,17 @@ module.exports = function(content) {
   
   this.addDependency(this.resourcePath);
 
-  if (config.rewritePath) {
-    let filePath;
+  const filePathArray = config.basePath.concat(fileName);
+  const filePath = JSON.stringify(filePathArray).slice(1, -1);
 
-    if (config.rewritePath === "./" || config.rewritePath === ".\\") {
-      filePath = JSON.stringify(config.rewritePath + fileName);
-    } else {
-      filePath = JSON.stringify(path.join(config.rewritePath, fileName));
-    }
-
-    return (
-      "try { global.process.dlopen(module, " +
-      filePath +
-      "); } " +
-      "catch(exception) { throw new Error('Cannot open ' + " +
-      filePath +
-      " + ': ' + exception); };"
-    );
-  } else {
-    const filePathArray = config.basePath.concat(fileName);
-    const filePath = JSON.stringify(filePathArray).slice(1, -1);
-
-    return (
-      "const path = require('path');" +
-      "const filePath = path.resolve(__dirname, " +
+  return (
+    "const path = require('path');" +
+      "const filePath = path.resolve(process.cwd(), " +
       filePath +
       ");" +
       "try { global.process.dlopen(module, filePath); } " +
       "catch(exception) { throw new Error('Cannot open ' + filePath + ': ' + exception); };"
-    );
-  }
+  );
 };
 
 module.exports.raw = true;
